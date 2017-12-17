301bd7632d69fcde91214c2b5ea8ebc1472fee19
柳生昌宏 <gyu7372@gmail.com>
2017-12-17 10:59:56 +0900

    [update] 1 app/javascript/packs/application.jsxの中身が表示されるように設定

--- app/javascript/packs/application.jsx
+++ app/javascript/packs/application.jsx
@@ -18,9 +18,7 @@ Hello.propTypes = {
   name: PropTypes.string
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+ReactDOM.render(
+  <Hello name="React" />,
+  document.getElementById('root'),
+)
